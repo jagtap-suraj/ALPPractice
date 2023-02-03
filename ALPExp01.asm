7f984c0c86e94391c58b2eea1caca7fdd58ceb63
Suraj Jagtap <suraj552003@gmail.com>
2023-02-03 11:29:34 +0530

    Exp 01

--- ALPExp01.asm
+++ ALPExp01.asm
@@ -1,9 +1,9 @@
 DATA SEGMENT
-    MSG DB 0DH, 0AH, "HELLO$"
+    MSG DB 0AH, 0DH, "hello suraj$"
     DATA ENDS
 
 CODE SEGMENT
-    ASSUME CS:CODE, DS:DATA
+    ASSUME CS: CODE, DS: DATA
     START:
         MOV AX, DATA
         MOV DS, AX
@@ -11,6 +11,7 @@ CODE SEGMENT
         LEA DX, MSG
         INT 21H
         MOV AH, 4CH
+        MOV AL, 00H
         INT 21H
     CODE ENDS
     END START
